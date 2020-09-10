33aa63b0c50a3d97f3abba0f62660004ee9ba388
Jackhalabardnik <jackowojcio@wp.pl>
2020-09-10 11:52:32 +0200

    bugfix(views.py): switch from __contains to __icontains to avoid ommiting capital letters in search

--- boards/views.py
+++ boards/views.py
@@ -64,7 +64,7 @@ class SearchTopicView(SearchView):
 
     def get_queryset(self):
         queryset = super().get_queryset().order_by('-last_updated').annotate(replies=Count('posts') - 1)
-        return queryset.filter(subject__contains=self.kwargs['search_phase'].replace('-',' '))
+        return queryset.filter(subject__icontains=self.kwargs['search_phase'].replace('-',' '))
 
 
 class PostListView(BasicSearchView):
@@ -98,7 +98,7 @@ class SearchPostView(SearchView):
 
     def get_queryset(self):
         queryset = super().get_queryset().order_by('created_at')
-        return queryset.filter(message__contains=self.kwargs['search_phase'].replace('-',' '))
+        return queryset.filter(message__icontains=self.kwargs['search_phase'].replace('-',' '))
 
 
 class NewTopicView(LoginRequiredMixin, CreateView):
